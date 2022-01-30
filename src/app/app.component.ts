38e3762ffa386385383e643af34600daff51fdf3
Boyan Zahariev <bzahariev@soft2run.com>
2022-01-31 00:02:36 +0200

    add: dynamic background from dice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,17 @@
-import { Component, ElementRef, ViewChild } from "@angular/core";
+import { Component, ElementRef, Renderer2, ViewChild } from "@angular/core";
 
 import { BehaviorSubject, Subject } from "rxjs";
 import { GameService } from "./game.service";
 
+enum diceNumbers {
+  "#465658",
+  "#f4c903",
+  "#fdae3e",
+  "#ec76a4",
+  "#9056bd",
+  "#5084ef",
+  "#34bbd2",
+}
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -10,8 +19,17 @@ import { GameService } from "./game.service";
 })
 export class AppComponent {
   @ViewChild("board") board!: ElementRef;
-
+  diceNum = diceNumbers;
   marginTop = 0;
 
-  constructor(public gs: GameService) {}
+  constructor(public gs: GameService, private renderer: Renderer2) {}
+
+  diceChange(diceNum: number): void {
+    if (diceNum === 0) return;
+    this.renderer.setStyle(
+      document.body,
+      "background-color",
+      this.diceNum[diceNum]
+    );
+  }
 }
